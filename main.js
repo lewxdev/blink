7699acf871ecd8d265eb7bf0608e2ae9f3141a2d
J. Lewis <inquiries.lewis+dev@gmail.com>
2019-12-17 11:47:54 -0500

    Created click handler for each column

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("we're linked up")
+/*
+
+0. Have a flag that keeps track of player turn
+1. Add a click handler for each column
+2. Make a function that places 1 red chip (in that column)
+
+*/
+
+let turn = 0
+const column = document.querySelectorAll(".column")
+
+column.forEach((element) => {
+	element.addEventListener("click", function () {
+		
+
+		turn = turn === 1 ? 0 : 1
+	})
+})
