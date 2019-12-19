8d2a3ef97b7a2de25ff7e22be453a15571f34ff3
Paul Racisz <paulieracisz@gmail.com>
2019-12-19 11:02:29 -0500

    adding negative diag check, needs debugging

--- main.js
+++ main.js
@@ -34,8 +34,8 @@ function initalGrid() {
                     if (checkWin(chipCoords)) {
                         alert(`Player ${turn + 1} Won!`)
                         game = initalGrid()
-					} else turn = turn === 0 ? 1 : 0
-					
+                    } else turn = turn === 0 ? 1 : 0
+
                     turnStatus.className = `turncolor${turn}`
                     break
                 }
@@ -50,21 +50,37 @@ function initalGrid() {
 }
 let game = initalGrid();
 
-document.querySelector(".close").onclick = function () {
-	document.querySelectorAll(".info, .close").forEach(element => element.classList.toggle("hidden"))
+document.querySelector(".close").onclick = function() {
+    document.querySelectorAll(".info, .close").forEach(element => element.classList.toggle("hidden"))
 }
 
 function checkWin(coordinates) {
     const chipCell = game[coordinates.x][coordinates.y]
     let vertStr = ""
+    let negDiagStr = ""
+    let win = false
 
     for (let yIndex = coordinates.y; yIndex >= 0; yIndex--) {
-		const currentCell = game[coordinates.x][yIndex].querySelector(".chip")
-		vertStr += currentCell.classList.contains("p0") ? 0 : 1
-	}
-
-	
-
-    if (vertStr.includes(turn === 0 ? "0000" : "1111"))
-        return true
+        const currentCell = game[coordinates.x][yIndex].querySelector(".chip")
+        vertStr += currentCell.classList.contains("p0") ? 0 : 1
+        console.log(vertStr)
+        if (vertStr.includes(turn === 0 ? "0000" : "1111")) {
+            return true
+        } else if (coordinates.y >= 3) {
+            // we only want to check for a negative diaganol win if the y is greater than 3
+            for (yIndex = coordinates.y; yIndex >= 0; yIndex--) {
+                // starting off the for loop by counting down from the y position of the current chip
+                const currentCell = game[coordinates.x][yIndex].querySelector(".chip")
+                    // grabbing the coords of the current chip
+                console.log(currentCell)
+                for (coordinates.x = 0; coordinates.x <= coordinates.y; coordinates.x++) {
+                    // at the same time, we want to be increasing the x coords that we are checking
+                    negDiagStr += currentCell.classList.contains("p0") ? 0 : 1
+                    if (negDiagStr.includes(turn === 0 ? "0000" : "1111")) {
+                        return true
+                    }
+                }
+            }
+        }
+    }
 }
