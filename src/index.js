0d6526df365c767b30aedd072be65da18ee44a52
J. Lewis <inquiries.lewis+dev@gmail.com>
2019-12-19 13:47:31 -0500

    Huge refactor and completed neg/posDiag condition

--- src/index.js
+++ src/index.js
@@ -3,95 +3,174 @@ const grid = document.querySelector("main#grid")
 let turn = 0
 
 function initalGrid() {
-    grid.innerHTML = null
-    const gridArr = []
+	grid.innerHTML = null
+	const gridArr = []
 
-    for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
+	for (let columnIndex = 0; columnIndex < 7; columnIndex++) {
 
-        const column = document.createElement("div")
-        column.className = "column"
-        column.setAttribute("tabindex", columnIndex + 1)
-        grid.appendChild(column)
+		const column = document.createElement("div")
+		column.className = "column"
+		column.setAttribute("tabindex", columnIndex + 1)
+		grid.appendChild(column)
 
-        gridArr.push([])
+		gridArr.push([])
 
-        for (let cellIndex = 0; cellIndex < 6; cellIndex++) {
-            const cell = document.createElement("div")
-            cell.className = "cell center"
-            column.appendChild(cell)
+		for (let cellIndex = 0; cellIndex < 6; cellIndex++) {
+			const cell = document.createElement("div")
+			cell.className = "cell center"
+			column.appendChild(cell)
 
-            gridArr[columnIndex].push(cell)
-        }
+			gridArr[columnIndex].push(cell)
+		}
 
-        column.onclick = () => {
-            for (let [index, cell] of Array.from(column.children).entries()) {
-                if (!cell.querySelector(".chip")) {
-                    const chip = document.createElement("div")
-                    chip.className = `chip p${turn}`
-                    cell.appendChild(chip)
-                    const chipCoords = { y: index, x: columnIndex }
+		column.onclick = () => {
+			for (let [index, cell] of Array.from(column.children).entries()) {
+				if (!cell.querySelector(".chip")) {
+					const chip = document.createElement("div")
+					chip.className = `chip p${turn}`
+					cell.appendChild(chip)
+					const chipCoords = { y: index, x: columnIndex }
 
-                    if (checkWin(chipCoords)) {
-                        alert(`Player ${turn + 1} Won!`)
-                        game = initalGrid()
-<<<<<<< HEAD:main.js
-                    } else turn = turn === 0 ? 1 : 0
-
-                    turnStatus.className = `turncolor${turn}`
-=======
+					if (checkWin(chipCoords)) {
+						alert(`Player ${turn + 1} Won!`)
+						gameGrid = initalGrid()
 					} else turn = turn === 0 ? 1 : 0
-					
-                    turnIndicator.className = `chip p${turn}`
->>>>>>> 81e977d93b7f2f2b3cf89e61419e05f15ce993a5:src/index.js
-                    break
-                }
-            }
-        }
-        column.onkeyup = (event) => {
-            if (event.key === "Enter") column.onclick()
-        }
-    }
-    turnIndicator.className = `chip p${turn}`
-    return gridArr
-}
-let game = initalGrid();
 
-<<<<<<< HEAD:main.js
-document.querySelector(".close").onclick = function() {
-    document.querySelectorAll(".info, .close").forEach(element => element.classList.toggle("hidden"))
-=======
+					turnIndicator.className = `chip p${turn}`
+					break
+				}
+			}
+		}
+		column.onkeyup = (event) => {
+			if (event.key === "Enter") column.onclick()
+		}
+	}
+	turnIndicator.className = `chip p${turn}`
+	return gridArr
+}
+let gameGrid = initalGrid();
+
 document.querySelector(".close").onclick = function () {
-	document.querySelectorAll(".info, .close, .content-container").forEach(element => element.classList.toggle("hidden"))
->>>>>>> 81e977d93b7f2f2b3cf89e61419e05f15ce993a5:src/index.js
+	document.querySelectorAll(".info").forEach(element => element.classList.toggle("hidden"))
 }
 
 function checkWin(coordinates) {
-    const chipCell = game[coordinates.x][coordinates.y]
-    let vertStr = ""
-    let negDiagStr = ""
-    let win = false
+	const checkVert = function () {
+		let vertStr = ""
 
-    for (let yIndex = coordinates.y; yIndex >= 0; yIndex--) {
-        const currentCell = game[coordinates.x][yIndex].querySelector(".chip")
-        vertStr += currentCell.classList.contains("p0") ? 0 : 1
-        console.log(vertStr)
-        if (vertStr.includes(turn === 0 ? "0000" : "1111")) {
-            return true
-        } else if (coordinates.y >= 3) {
-            // we only want to check for a negative diaganol win if the y is greater than 3
-            for (yIndex = coordinates.y; yIndex >= 0; yIndex--) {
-                // starting off the for loop by counting down from the y position of the current chip
-                const currentCell = game[coordinates.x][yIndex].querySelector(".chip")
-                    // grabbing the coords of the current chip
-                console.log(currentCell)
-                for (coordinates.x = 0; coordinates.x <= coordinates.y; coordinates.x++) {
-                    // at the same time, we want to be increasing the x coords that we are checking
-                    negDiagStr += currentCell.classList.contains("p0") ? 0 : 1
-                    if (negDiagStr.includes(turn === 0 ? "0000" : "1111")) {
-                        return true
-                    }
-                }
-            }
-        }
-    }
+		let yIndex = coordinates.y
+		while (yIndex >= 0) {
+			let currentChip = gameGrid[coordinates.x][yIndex].querySelector(".chip")
+
+			if (currentChip.classList.contains("p0")) currentChip = "0"
+			else currentChip = "1"
+
+			vertStr += currentChip
+			yIndex--
+		}
+
+		if (vertStr.includes("0000") || vertStr.includes("1111"))
+			return true
+	}()
+
+	const checkHorz = function () {
+		let horzStr = ""
+
+		let yIndex = coordinates.y
+		while (yIndex >= 0) {
+			let currentChip = gameGrid[coordinates.x][yIndex].querySelector(".chip")
+
+			if (currentChip.classList.contains("p0")) currentChip = "0"
+			else currentChip = "1"
+
+			horzStr += currentChip
+			yIndex--
+		}
+
+		if (horzStr.includes("0000") || horzStr.includes("1111"))
+			return true
+	}()
+
+	const checkNegDiag = function () {
+		let negDiagStr = ""
+		
+		down: {
+			let xIndex = coordinates.x,
+				yIndex = coordinates.y
+			while (yIndex >= 0 && xIndex < gameGrid.length) {
+				let currentChip = gameGrid[xIndex][yIndex].querySelector(".chip")
+
+				if (!currentChip) currentChip = "N"
+				else if (currentChip.classList.contains("p0")) currentChip = "0"
+				else currentChip = "1"
+
+				negDiagStr += currentChip
+
+				yIndex--
+				xIndex++
+			}
+		}
+		up: {
+			let xIndex = coordinates.x - 1,
+				yIndex = coordinates.y + 1
+			while (yIndex < gameGrid[0].length && xIndex >= 0 ) {
+				let currentChip = gameGrid[xIndex][yIndex].querySelector(".chip")
+
+				if (!currentChip) currentChip = "N"
+				else if (currentChip.classList.contains("p0")) currentChip = "0"
+				else currentChip = "1"
+
+				negDiagStr = currentChip + negDiagStr
+
+				yIndex++
+				xIndex--
+			}
+		}
+
+		if (negDiagStr.includes("0000") || negDiagStr.includes("1111"))
+			return true
+	}()
+
+	const checkPosDiag = function () {
+		let posDiagStr = ""
+		
+		down: {
+			let xIndex = coordinates.x,
+				yIndex = coordinates.y
+			while (yIndex >= 0 && xIndex >= 0) {
+				let currentChip = gameGrid[xIndex][yIndex].querySelector(".chip")
+
+				if (!currentChip) currentChip = "N"
+				else if (currentChip.classList.contains("p0")) currentChip = "0"
+				else currentChip = "1"
+
+				posDiagStr += currentChip
+
+				yIndex--
+				xIndex--
+			}
+		}
+		up: {
+			let xIndex = coordinates.x + 1,
+				yIndex = coordinates.y + 1
+			while (yIndex < gameGrid[0].length && xIndex < gameGrid.length ) {
+				let currentChip = gameGrid[xIndex][yIndex].querySelector(".chip")
+
+				if (!currentChip) currentChip = "N"
+				else if (currentChip.classList.contains("p0")) currentChip = "0"
+				else currentChip = "1"
+
+				posDiagStr = currentChip + posDiagStr
+
+				yIndex++
+				xIndex++
+			}
+		}
+
+		if (posDiagStr.includes("0000") || posDiagStr.includes("1111"))
+			return true
+	}()
+
+	if (checkVert || checkHorz || checkNegDiag || checkPosDiag)
+		return true
 }
